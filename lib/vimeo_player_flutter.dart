c37830c38780e6e73a67f327d1df9a41bce45412
Kelvin Spencer <kelvin.spencer@gmail.com>
2021-11-07 17:42:45 -0500

    Update vimeo_player_flutter.dart

--- lib/vimeo_player_flutter.dart
+++ lib/vimeo_player_flutter.dart
@@ -66,7 +66,7 @@ class VimeoPlayer extends StatelessWidget {
 //                 width="100%" height="100%" frameborder="0" allow="fullscreen" 
 //                 allowfullscreen></iframe>
                    <br>
-                   <iframe src="https://vimeo.com/event/1463936/chat/" width="100%" height="600" frameborder="0"></iframe>
+                   <iframe src="https://vimeo.com/event/$videoId/chat/" width="100%" height="600" frameborder="0"></iframe>
              </body>
             </html>
             ''';
