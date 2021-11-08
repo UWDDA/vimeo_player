508c0e4730e2c3327a668662e8bdc770263b1051
Kelvin Spencer <kelvin.spencer@gmail.com>
2021-11-07 19:30:32 -0500

    Update vimeo_player_flutter.dart

--- lib/vimeo_player_flutter.dart
+++ lib/vimeo_player_flutter.dart
@@ -75,3 +75,53 @@ class VimeoPlayer extends StatelessWidget {
     return 'data:text/html;base64,$contentBase64';
   }
 }
+
+
+///vimeo player for Flutter apps
+///Flutter plugin based on the [webview_flutter] plugin
+///[videoId] is the only required field to use this plugin
+
+class VimeoPlayerChat extends StatelessWidget {
+  final String videoId;
+
+  ///constructor
+  const VimeoPlayerChat({
+    Key? key,
+    required this.videoId,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: _videoChat(this.videoId),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+
+  ///web page containing iframe of the vimeo video chat
+  String _videoChat(String videoId) {
+    final html = '''
+            <html>
+              <head>
+                <style>
+                  body {
+                   background-color: lightgray;
+                   margin: 0px;
+                   }
+                </style>
+                <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0">
+                <meta http-equiv="Content-Security-Policy" 
+                content="default-src * gap:; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src *; 
+                img-src * data: blob: android-webview-video-poster:; style-src * 'unsafe-inline';">
+             </head>
+             <body>
+                   <iframe src="https://vimeo.com/event/$videoId/chat/" width="100%" height="600" frameborder="0"></iframe>
+             </body>
+            </html>
+            ''';
+    final String contentBase64 =
+        base64Encode(const Utf8Encoder().convert(html));
+    return 'data:text/html;base64,$contentBase64';
+  }
+}
+
